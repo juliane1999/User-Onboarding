8c2c346b9cf288c0663c61639abda357b8229c44
juliane1999 <lastjuliane@gmail.com>
2021-07-21 19:21:23 -0700

    completed top half of app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios from 'axios'
+import React, {useState,useEffect} from 'react'
 import Form from './Form'
+import Schema from './Schema'
+import {reach} from 'yup'
+
+const initialFormValues = {
+  username:'',
+  email:'', 
+  password:'',
+  terms:'',
+}
+
+const initialFormErrors = {
+  username:'',
+  email:'',
+  password:'',
+  terms:'',
+}
+
+const initialUsers = []
+const initialDisabled = true
 
 function App() {
+
+  const [user,setUser] = useState(initialUsers) ;
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors) ;
+  const [disabled, setDisabled] = useState(initialDisabled);  
+
+  const getUsers = () => {
+    axios.get('https://reqres.in/api/users')
+    .then(res => {
+      setUser(res.data)
+      console.log(res.data)
+      .catch(err => {
+        console.log(err)
+      })
+    })
+  }
+
+  const postNewUser = newUser => {
+    axios.post('https://reqres.in/api/users', newUser)
+    .then(res => {
+      setUser([res.data,...user])
+    })
+    .catch(err => {
+      console.log(err)
+    })
+    .finally(() => {
+      setFormValues(initialFormValues)
+    })
+  }
+
+  const validate = (name,value) => {
+    reach(Schema,name)
+    validate(value)
+    .then((valid) => setFormErrors({...formErrors,[name]: ''}))
+    .catch(err => setFormErrors({...formErrors,[name]: err.errors[0]}))
+  }
+
+  const inputChange = (name, value) => {
+    validate(name,value)
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  const formSubmit = () => {
+    const newUser = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+    }
+    postNewUser(newUser)
+  }
+
+  useEffect(() => {
+    Schema.isValid(formValues)
+    .then(valid => setDisabled(!valid))
+  }, [formValues])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header><h1>Users</h1></header>
+     
     </div>
   );
 }
